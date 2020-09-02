dad59af2e29a227e0d35d19fb33366c38285008c
fryday007 <61014780+fryday007@users.noreply.github.com>
2020-09-02 22:30:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from hmmlearn import hmm
 import numpy as np
 import math
 from state import STATE_MATRIX
-from observation import emm_dict, OBS_MATRIX
+from observation import emm_dict, OBS_MATRIX ,obs_arr
 
 
 model = hmm.MultinomialHMM(n_components=27)
@@ -10,7 +10,7 @@ model.startprob_ = np.ones(27) / 27
 model.transmat_ = STATE_MATRIX
 model.emissionprob_ = OBS_MATRIX
 
-logprob, seq = model.decode(np.array([[0, 0, 0, 2, 1, 0]]).transpose())
+logprob, seq = model.decode(np.array([obs_arr[18:]]).transpose())
 
 print("math.exp(logprob) = ", math.exp(logprob))
 print("seq = ", seq)
